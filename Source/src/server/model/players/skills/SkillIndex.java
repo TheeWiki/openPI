016e5abb1e8a4ebe917f63dffa7f5c81c65bac1b
TheeWiki <rare_m3@hotmail.com>
2016-02-06 21:43:38 -0500

    Skill Index enum class

---
+++ Source/src/server/model/players/skills/SkillIndex.java
@@ -0,0 +1,35 @@
+package server.model.players.skills;
+
+public enum SkillIndex 
+{
+	ATTACK(0),
+	DEFENCE(1),
+	STRENGTH(2),
+	HITPOINTS(3),
+	RANGE(4),
+	PRAYER(5),
+	MAGIC(6);
+	
+	private int skillID;
+	
+	public int getSkillID()
+	{
+		return skillID;
+	}
+	
+	SkillIndex(int skillID)
+	{
+		this.skillID = skillID;
+	}
+	
+	public static SkillIndex getSkill(int skill)
+	{
+		for (SkillIndex index : values()) {
+			if (skill == index.getSkillID())
+			{
+				return index;
+			}
+		}
+		return null;
+	}
+}
