e43bdf2cb71a5ef2a6f62ae460409ec130f3b86f
TheeWiki <rare_m3@hotmail.com>
2016-02-09 18:38:33 -0500

    Fixed actions not being registered in actionhandler

--- Source/src/server/model/players/ActionHandler.java
+++ Source/src/server/model/players/ActionHandler.java
@@ -2,64 +2,56 @@ package server.model.players;
 
 import server.Server;
 import server.model.objects.Object;
-import server.util.ScriptManager;
 import server.world.locations.LocationController;
 
 public class ActionHandler {
-	
+
 	private Client c;
-	
+
 	public ActionHandler(Client Client) {
 		this.c = Client;
 	}
-	
-	
+
 	public void firstClickObject(int objectType, int obX, int obY) {
 		c.clickObjectType = 0;
-		if(c.actionTimer > 0) {
-			return;
-		}
-		c.actionTimer = 4;
 		LocationController.sendFirstClickObject(c, objectType);
 		c.sendMessage("[object 1] - " + objectType);
-		switch(objectType) {
- 
- 	/**
+		switch (objectType) {
+
+		/**
 		 * Entering the Fight Caves.
 		 */
 		case 9356:
 			c.getPA().enterCaves();
 			c.sendMessage("Best of luck in the waves!");
 			c.sendMessage("If something bugs, just relog!");
-		break;
-				
-		
+			break;
+
 		/**
-		 * Clicking on the Ancient Altar. 
+		 * Clicking on the Ancient Altar.
 		 */
 		case 6552:
-		if (c.playerMagicBook == 0) {
-                        c.playerMagicBook = 1;
-                        c.setSidebarInterface(6, 12855);
-                        c.autocasting = false;
-                        c.sendMessage("An ancient wisdomin fills your mind.");
-                        c.getPA().resetAutocast();
-		} else {
-			c.setSidebarInterface(6, 1151); 
-			c.playerMagicBook = 0;
-                        c.autocasting = false;
-			c.sendMessage("You feel a drain on your memory.");
-			c.autocastId = -1;
-			c.getPA().resetAutocast();
-		}	
-		break;
-		
-		
-        /**
-         * Recharing prayer points.
-         */
+			if (c.playerMagicBook == 0) {
+				c.playerMagicBook = 1;
+				c.setSidebarInterface(6, 12855);
+				c.autocasting = false;
+				c.sendMessage("An ancient wisdomin fills your mind.");
+				c.getPA().resetAutocast();
+			} else {
+				c.setSidebarInterface(6, 1151);
+				c.playerMagicBook = 0;
+				c.autocasting = false;
+				c.sendMessage("You feel a drain on your memory.");
+				c.autocastId = -1;
+				c.getPA().resetAutocast();
+			}
+			break;
+
+		/**
+		 * Recharing prayer points.
+		 */
 		case 409:
-			if(c.playerLevel[5] < c.getPA().getLevelForXP(c.playerXP[5])) {
+			if (c.playerLevel[5] < c.getPA().getLevelForXP(c.playerXP[5])) {
 				c.startAnimation(645);
 				c.playerLevel[5] = c.getPA().getLevelForXP(c.playerXP[5]);
 				c.sendMessage("You recharge your prayer points.");
@@ -68,8 +60,7 @@ public class ActionHandler {
 				c.sendMessage("You already have full prayer points.");
 			}
 			break;
-		
-		
+
 		/**
 		 * Opening the bank when clicking on bank booths, etc.
 		 */
@@ -78,9 +69,8 @@ public class ActionHandler {
 		case 11758:
 		case 3193:
 			c.getPA().openUpBank();
-		break;
-		
-		
+			break;
+
 		/**
 		 * Aquring god capes.
 		 */
@@ -89,24 +79,23 @@ public class ActionHandler {
 				c.startAnimation(645);
 				c.sendMessage("Saradomin blesses you with a cape.");
 				c.getItems().addItem(2412, 1);
-			}	
-		break;
+			}
+			break;
 		case 2875:
 			if (!c.getItems().ownsCape()) {
 				c.startAnimation(645);
 				c.sendMessage("Guthix blesses you with a cape.");
 				c.getItems().addItem(2413, 1);
 			}
-		break;
+			break;
 		case 2874:
 			if (!c.getItems().ownsCape()) {
 				c.startAnimation(645);
 				c.sendMessage("Zamorak blesses you with a cape.");
 				c.getItems().addItem(2414, 1);
 			}
-		break;
-		
-		
+			break;
+
 		/**
 		 * Oblisks in the wilderness.
 		 */
@@ -117,111 +106,110 @@ public class ActionHandler {
 		case 14826:
 		case 14831:
 			Server.objectManager.startObelisk(objectType);
-		break;
-		
-		
+			break;
+
 		/**
 		 * Clicking certain doors.
 		 */
 		case 6749:
-			if(obX == 3562 && obY == 9678) {
+			if (obX == 3562 && obY == 9678) {
 				c.getPA().object(3562, 9678, 6749, -3, 0);
 				c.getPA().object(3562, 9677, 6730, -1, 0);
-			} else if(obX == 3558 && obY == 9677) {
+			} else if (obX == 3558 && obY == 9677) {
 				c.getPA().object(3558, 9677, 6749, -1, 0);
 				c.getPA().object(3558, 9678, 6730, -3, 0);
 			}
 			break;
-			
+
 		case 6730:
-			if(obX == 3558 && obY == 9677) {
+			if (obX == 3558 && obY == 9677) {
 				c.getPA().object(3562, 9678, 6749, -3, 0);
 				c.getPA().object(3562, 9677, 6730, -1, 0);
-			} else if(obX == 3558 && obY == 9678) {
+			} else if (obX == 3558 && obY == 9678) {
 				c.getPA().object(3558, 9677, 6749, -1, 0);
 				c.getPA().object(3558, 9678, 6730, -3, 0);
 			}
 			break;
-			
+
 		case 6727:
-			if(obX == 3551 && obY == 9684) {
+			if (obX == 3551 && obY == 9684) {
 				c.sendMessage("You cant open this door..");
 			}
 			break;
-			
+
 		case 6746:
-			if(obX == 3552 && obY == 9684) {
+			if (obX == 3552 && obY == 9684) {
 				c.sendMessage("You cant open this door..");
 			}
 			break;
-			
+
 		case 6748:
-			if(obX == 3545 && obY == 9678) {
+			if (obX == 3545 && obY == 9678) {
 				c.getPA().object(3545, 9678, 6748, -3, 0);
 				c.getPA().object(3545, 9677, 6729, -1, 0);
-			} else if(obX == 3541 && obY == 9677) {
+			} else if (obX == 3541 && obY == 9677) {
 				c.getPA().object(3541, 9677, 6748, -1, 0);
 				c.getPA().object(3541, 9678, 6729, -3, 0);
 			}
 			break;
-			
+
 		case 6729:
-			if(obX == 3545 && obY == 9677){
+			if (obX == 3545 && obY == 9677) {
 				c.getPA().object(3545, 9678, 6748, -3, 0);
 				c.getPA().object(3545, 9677, 6729, -1, 0);
-			} else if(obX == 3541 && obY == 9678) {
+			} else if (obX == 3541 && obY == 9678) {
 				c.getPA().object(3541, 9677, 6748, -1, 0);
 				c.getPA().object(3541, 9678, 6729, -3, 0);
 			}
 			break;
-			
+
 		case 6726:
-			if(obX == 3534 && obY == 9684) {
+			if (obX == 3534 && obY == 9684) {
 				c.getPA().object(3534, 9684, 6726, -4, 0);
 				c.getPA().object(3535, 9684, 6745, -2, 0);
-			} else if(obX == 3535 && obY == 9688) {
+			} else if (obX == 3535 && obY == 9688) {
 				c.getPA().object(3535, 9688, 6726, -2, 0);
 				c.getPA().object(3534, 9688, 6745, -4, 0);
 			}
 			break;
-			
+
 		case 6745:
-			if(obX == 3535 && obY == 9684) {
+			if (obX == 3535 && obY == 9684) {
 				c.getPA().object(3534, 9684, 6726, -4, 0);
 				c.getPA().object(3535, 9684, 6745, -2, 0);
-			} else if(obX == 3534 && obY == 9688) {
+			} else if (obX == 3534 && obY == 9688) {
 				c.getPA().object(3535, 9688, 6726, -2, 0);
 				c.getPA().object(3534, 9688, 6745, -4, 0);
 			}
 			break;
-			
+
 		case 6743:
-			if(obX == 3545 && obY == 9695) {
+			if (obX == 3545 && obY == 9695) {
 				c.getPA().object(3545, 9694, 6724, -1, 0);
 				c.getPA().object(3545, 9695, 6743, -3, 0);
-			} else if(obX == 3541 && obY == 9694) {
+			} else if (obX == 3541 && obY == 9694) {
 				c.getPA().object(3541, 9694, 6724, -1, 0);
 				c.getPA().object(3541, 9695, 6743, -3, 0);
 			}
 			break;
-			
+
 		case 6724:
-			if(obX == 3545 && obY == 9694) {
+			if (obX == 3545 && obY == 9694) {
 				c.getPA().object(3545, 9694, 6724, -1, 0);
 				c.getPA().object(3545, 9695, 6743, -3, 0);
-			} else if(obX == 3541 && obY == 9695) {
+			} else if (obX == 3541 && obY == 9695) {
 				c.getPA().object(3541, 9694, 6724, -1, 0);
 				c.getPA().object(3541, 9695, 6743, -3, 0);
 			}
-			break; 
-			
+			break;
+
 		case 1516:
 		case 1519:
 			if (c.objectY == 9698) {
 				if (c.absY >= c.objectY)
-					c.getPA().walkTo(0,-1);
+					c.getPA().walkTo(0, -1);
 				else
-					c.getPA().walkTo(0,1);
+					c.getPA().walkTo(0, 1);
 				break;
 			}
 		case 1530:
@@ -235,23 +223,23 @@ public class ActionHandler {
 		case 6725:
 		case 3198:
 		case 3197:
-			Server.objectHandler.doorHandling(objectType, c.objectX, c.objectY, 0);	
+			Server.objectHandler.doorHandling(objectType, c.objectX, c.objectY, 0);
 			break;
-		
+
 		case 9319:
 			if (c.heightLevel == 0)
 				c.getPA().movePlayer(c.absX, c.absY, 1);
 			else if (c.heightLevel == 1)
 				c.getPA().movePlayer(c.absX, c.absY, 2);
-		break;
-		
+			break;
+
 		case 9320:
 			if (c.heightLevel == 1)
 				c.getPA().movePlayer(c.absX, c.absY, 0);
 			else if (c.heightLevel == 2)
 				c.getPA().movePlayer(c.absX, c.absY, 1);
-		break;
-		
+			break;
+
 		case 4496:
 		case 4494:
 			if (c.heightLevel == 2) {
@@ -259,114 +247,110 @@ public class ActionHandler {
 			} else if (c.heightLevel == 1) {
 				c.getPA().movePlayer(c.absX + 5, c.absY, 0);
 			}
-		break;
-		
+			break;
+
 		case 4493:
 			if (c.heightLevel == 0) {
 				c.getPA().movePlayer(c.absX - 5, c.absY, 1);
 			} else if (c.heightLevel == 1) {
 				c.getPA().movePlayer(c.absX + 5, c.absY, 2);
 			}
-		break;
-		
+			break;
+
 		case 4495:
 			if (c.heightLevel == 1) {
 				c.getPA().movePlayer(c.absX + 5, c.absY, 2);
 			}
-		break;
-		
+			break;
+
 		case 5126:
 			if (c.absY == 3554)
-				c.getPA().walkTo(0,1);
+				c.getPA().walkTo(0, 1);
 			else
-				c.getPA().walkTo(0,-1);
-		break;
-		
+				c.getPA().walkTo(0, -1);
+			break;
+
 		case 1759:
 			if (c.objectX == 2884 && c.objectY == 3397)
-				c.getPA().movePlayer(c.absX, c.absY + 6400, 0);				
-		break;
+				c.getPA().movePlayer(c.absX, c.absY + 6400, 0);
+			break;
 		case 1558:
 			if (c.absX == 3041 && c.absY == 10308) {
-                            c.getPA().movePlayer(3040, 10308, 0);	
-                        } else if(c.absX == 3040 && c.absY == 10308) {
-                                  c.getPA().movePlayer(3041, 10308, 0);
-                        } else if(c.absX == 3040 && c.absY == 10307) {
-                                  c.getPA().movePlayer(3041, 10307, 0);
-                        } else if(c.absX == 3041 && c.absY == 10307) {
-                                  c.getPA().movePlayer(3040, 10307, 0);
-                        } else if(c.absX == 3044 && c.absY == 10341) {
-                                  c.getPA().movePlayer(3045, 10341, 0);
-                        } else if(c.absX == 3045 && c.absY == 10341) {
-                                  c.getPA().movePlayer(3044, 10341, 0);
-                        } else if(c.absX == 3044 && c.absY == 10342) {
-                                  c.getPA().movePlayer(3045, 10342, 0);
-                        } else if(c.absX == 3045 && c.absY == 10342) {
-                                  c.getPA().movePlayer(3044, 10343, 0);
-                        }
-		break;
+				c.getPA().movePlayer(3040, 10308, 0);
+			} else if (c.absX == 3040 && c.absY == 10308) {
+				c.getPA().movePlayer(3041, 10308, 0);
+			} else if (c.absX == 3040 && c.absY == 10307) {
+				c.getPA().movePlayer(3041, 10307, 0);
+			} else if (c.absX == 3041 && c.absY == 10307) {
+				c.getPA().movePlayer(3040, 10307, 0);
+			} else if (c.absX == 3044 && c.absY == 10341) {
+				c.getPA().movePlayer(3045, 10341, 0);
+			} else if (c.absX == 3045 && c.absY == 10341) {
+				c.getPA().movePlayer(3044, 10341, 0);
+			} else if (c.absX == 3044 && c.absY == 10342) {
+				c.getPA().movePlayer(3045, 10342, 0);
+			} else if (c.absX == 3045 && c.absY == 10342) {
+				c.getPA().movePlayer(3044, 10343, 0);
+			}
+			break;
 		case 1557:
 			if (c.absX == 3023 && c.absY == 10312) {
-                            c.getPA().movePlayer(3022, 10312, 0);	
-                        } else if(c.absX == 3022 && c.absY == 10312) {
-                                  c.getPA().movePlayer(3023, 10312, 0);
-                        } else if(c.absX == 3023 && c.absY == 10311) {
-                                  c.getPA().movePlayer(3022, 10311, 0);
-                        } else if(c.absX == 3022 && c.absY == 10311) {
-                                  c.getPA().movePlayer(3023, 10311, 0);
-                        }
-		break;
+				c.getPA().movePlayer(3022, 10312, 0);
+			} else if (c.absX == 3022 && c.absY == 10312) {
+				c.getPA().movePlayer(3023, 10312, 0);
+			} else if (c.absX == 3023 && c.absY == 10311) {
+				c.getPA().movePlayer(3022, 10311, 0);
+			} else if (c.absX == 3022 && c.absY == 10311) {
+				c.getPA().movePlayer(3023, 10311, 0);
+			}
+			break;
 		case 2558:
-			c.sendMessage("This door is locked.");	
-		break;
-		
+			c.sendMessage("This door is locked.");
+			break;
+
 		case 9294:
 			if (c.absX < c.objectX) {
 				c.getPA().movePlayer(c.objectX + 1, c.absY, 0);
 			} else if (c.absX > c.objectX) {
 				c.getPA().movePlayer(c.objectX - 1, c.absY, 0);
 			}
-		break;
-		
+			break;
+
 		case 9293:
 			if (c.absX < c.objectX) {
 				c.getPA().movePlayer(2892, 9799, 0);
 			} else {
 				c.getPA().movePlayer(2886, 9799, 0);
 			}
-		break;
-		
+			break;
+
 		case 10529:
 		case 10527:
 			if (c.absY <= c.objectY)
-				c.getPA().walkTo(0,1);
+				c.getPA().walkTo(0, 1);
 			else
-				c.getPA().walkTo(0,-1);
-		break;
-	
+				c.getPA().walkTo(0, -1);
+			break;
+
 		case 733:
 			c.startAnimation(451);
-		
+
 			if (c.objectX == 3158 && c.objectY == 3951) {
 				new Object(734, c.objectX, c.objectY, c.heightLevel, 1, 10, 733, 50);
 			} else {
 				new Object(734, c.objectX, c.objectY, c.heightLevel, 0, 10, 733, 50);
 			}
-		break;
-		
-//		default:
-//			ScriptManager.callFunc("objectClick1_"+objectType, c, objectType, obX, obY);
-//			break;
-//			
+			break;
+
 		}
 	}
-	
+
 	public void secondClickObject(int objectType, int obX, int obY) {
 		c.clickObjectType = 0;
+		c.sendMessage("[object 2] -  " + objectType);
 		LocationController.sendSecondClickObject(c, objectType);
-		switch(objectType) {
-			
-			
+		switch (objectType) {
+
 		/**
 		 * Opening the bank.
 		 */
@@ -374,55 +358,52 @@ public class ActionHandler {
 		case 14367:
 		case 11758:
 			c.getPA().openUpBank();
-		break;
-			
+			break;
+
 		}
 	}
-	
-	
+
 	public void thirdClickObject(int objectType, int obX, int obY) {
 		c.clickObjectType = 0;
-		c.sendMessage("Object type: " + objectType);
+		c.sendMessage("[object 3] -  " + objectType);
 		LocationController.sendThirdClickObject(c, objectType);
-		switch(objectType) {
-		default:
-			ScriptManager.callFunc("objectClick3_"+objectType, c, objectType, obX, obY);
-			break;
+		switch (objectType) {
 		}
 	}
-	
+
 	public void firstClickNpc(int npcType) {
 		c.clickNpcType = 0;
 		c.npcClickIndex = 0;
+		c.sendMessage("[npc 1] - " + npcType);
 		LocationController.sendFirstClickNpc(c, npcType);
 		switch (npcType) {
-		
-		
-        /**
-         * Shops.
-         */
+
+		/**
+		 * Shops.
+		 */
 		case 2566:
 			c.getShops().openSkillCape();
-		break;
-		
+			break;
+
 		}
 	}
 
 	public void secondClickNpc(int npcType) {
 		c.clickNpcType = 0;
 		c.npcClickIndex = 0;
+		c.sendMessage("[npc 2] - " + npcType);
 		LocationController.sendSecondClickNpc(c, npcType);
-		switch(npcType) {
-		
-			
+		switch (npcType) {
+
 		}
 	}
-	
+
 	public void thirdClickNpc(int npcType) {
 		c.clickNpcType = 0;
 		c.npcClickIndex = 0;
+		c.sendMessage("[npc 3] - " + npcType);
 		LocationController.sendThirdClickNpc(c, npcType);
-		switch(npcType) {
+		switch (npcType) {
 
 		}
 	}
