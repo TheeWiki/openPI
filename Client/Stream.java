e27eda7969b3e77a069a398001553725f9f873cd
TheeWiki <rare_m3@hotmail.com>
2016-02-09 00:23:29 -0500

    Added more methods

--- Client/Stream.java
+++ Client/Stream.java
@@ -1,20 +1,17 @@
-// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
-
 import java.math.BigInteger;
+
 import sign.signlink;
 
 public final class Stream extends NodeSub {
 
 	public static Stream create() {
-		synchronized(nodeList) {
+		synchronized (nodeList) {
 			Stream stream = null;
-			if(anInt1412 > 0) {
+			if (anInt1412 > 0) {
 				anInt1412--;
 				stream = (Stream) nodeList.popHead();
 			}
-			if(stream != null) {
+			if (stream != null) {
 				stream.currentOffset = 0;
 				return stream;
 			}
@@ -25,87 +22,157 @@ public final class Stream extends NodeSub {
 		return stream_1;
 	}
 
-	private Stream() {
+	public Stream() {
+	}
+
+	public byte[] getData(byte[] buffer2) {
+		for (int i = 0; i < buffer2.length; i++)
+			buffer2[i] = buffer[currentOffset++];
+
+		return buffer2;
 	}
 
 	public Stream(byte abyte0[]) {
 		buffer = abyte0;
-			currentOffset = 0;
+		currentOffset = 0;
 	}
 
 	public void createFrame(int i) {
-		//System.out.println("Frame: " + i);
-		buffer[currentOffset++] = (byte)(i + encryption.getNextKey());
+		buffer[currentOffset++] = (byte) (i + encryption.getNextKey());
 	}
 
 	public void writeWordBigEndian(int i) {
-		buffer[currentOffset++] = (byte)i;
+		buffer[currentOffset++] = (byte) i;
+	}
+
+	final int v(int i) {
+		currentOffset += 3;
+		return (0xff & buffer[currentOffset - 3] << 16)
+				+ (0xff & buffer[currentOffset - 2] << 8)
+				+ (0xff & buffer[currentOffset - 1]);
+	}
+
+	public int readUnsignedWord2() {
+		return 1795;
+	}
+
+	public int readUnsignedByte2() {
+		return -263;
+	}
+
+	public int g2() {
+		currentOffset += 2;
+		return ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] & 0xff);
+	}
+
+	public int g4() {
+		currentOffset += 4;
+		return ((buffer[currentOffset - 4] & 0xff) << 24)
+				+ ((buffer[currentOffset - 3] & 0xff) << 16)
+				+ ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] & 0xff);
+	}
+
+	public int readShort2() {
+		currentOffset += 2;
+		int i = ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] & 0xff);
+		if (i > 60000)
+			i = -65535 + i;
+		return i;
+
+	}
+
+	public int readUSmart2() {
+		int baseVal = 0;
+		int lastVal = 0;
+		while ((lastVal = method422()) == 32767) {
+			baseVal += 32767;
+		}
+		return baseVal + lastVal;
+	}
+
+	public String readNewString() {
+		int i = currentOffset;
+		while (buffer[currentOffset++] != 0)
+			;
+		return new String(buffer, i, currentOffset - i - 1);
 	}
 
 	public void writeWord(int i) {
-		buffer[currentOffset++] = (byte)(i >> 8);
-		buffer[currentOffset++] = (byte)i;
+		buffer[currentOffset++] = (byte) (i >> 8);
+		buffer[currentOffset++] = (byte) i;
 	}
 
 	public void method400(int i) {
-		buffer[currentOffset++] = (byte)i;
-		buffer[currentOffset++] = (byte)(i >> 8);
+		buffer[currentOffset++] = (byte) i;
+		buffer[currentOffset++] = (byte) (i >> 8);
 	}
 
 	public void writeDWordBigEndian(int i) {
-		buffer[currentOffset++] = (byte)(i >> 16);
-		buffer[currentOffset++] = (byte)(i >> 8);
-		buffer[currentOffset++] = (byte)i;
+		buffer[currentOffset++] = (byte) (i >> 16);
+		buffer[currentOffset++] = (byte) (i >> 8);
+		buffer[currentOffset++] = (byte) i;
 	}
 
 	public void writeDWord(int i) {
-		buffer[currentOffset++] = (byte)(i >> 24);
-		buffer[currentOffset++] = (byte)(i >> 16);
-		buffer[currentOffset++] = (byte)(i >> 8);
-		buffer[currentOffset++] = (byte)i;
+		buffer[currentOffset++] = (byte) (i >> 24);
+		buffer[currentOffset++] = (byte) (i >> 16);
+		buffer[currentOffset++] = (byte) (i >> 8);
+		buffer[currentOffset++] = (byte) i;
 	}
 
 	public void method403(int j) {
-		buffer[currentOffset++] = (byte)j;
-		buffer[currentOffset++] = (byte)(j >> 8);
-		buffer[currentOffset++] = (byte)(j >> 16);
-		buffer[currentOffset++] = (byte)(j >> 24);
+		buffer[currentOffset++] = (byte) j;
+		buffer[currentOffset++] = (byte) (j >> 8);
+		buffer[currentOffset++] = (byte) (j >> 16);
+		buffer[currentOffset++] = (byte) (j >> 24);
 	}
 
 	public void writeQWord(long l) {
 		try {
-			buffer[currentOffset++] = (byte)(int)(l >> 56);
-			buffer[currentOffset++] = (byte)(int)(l >> 48);
-			buffer[currentOffset++] = (byte)(int)(l >> 40);
-			buffer[currentOffset++] = (byte)(int)(l >> 32);
-			buffer[currentOffset++] = (byte)(int)(l >> 24);
-			buffer[currentOffset++] = (byte)(int)(l >> 16);
-			buffer[currentOffset++] = (byte)(int)(l >> 8);
-			buffer[currentOffset++] = (byte)(int)l;
-		} catch(RuntimeException runtimeexception) {
-			signlink.reporterror("14395, " + 5 + ", " + l + ", " + runtimeexception.toString());
+			buffer[currentOffset++] = (byte) (int) (l >> 56);
+			buffer[currentOffset++] = (byte) (int) (l >> 48);
+			buffer[currentOffset++] = (byte) (int) (l >> 40);
+			buffer[currentOffset++] = (byte) (int) (l >> 32);
+			buffer[currentOffset++] = (byte) (int) (l >> 24);
+			buffer[currentOffset++] = (byte) (int) (l >> 16);
+			buffer[currentOffset++] = (byte) (int) (l >> 8);
+			buffer[currentOffset++] = (byte) (int) l;
+		} catch (RuntimeException runtimeexception) {
+			signlink.reporterror("14395, " + 5 + ", " + l + ", "
+					+ runtimeexception.toString());
 			throw new RuntimeException();
 		}
 	}
 
 	public void writeString(String s) {
-		//s.getBytes(0, s.length(), buffer, currentOffset);	//deprecated
+		// s.getBytes(0, s.length(), buffer, currentOffset); //deprecated
 		System.arraycopy(s.getBytes(), 0, buffer, currentOffset, s.length());
 		currentOffset += s.length();
 		buffer[currentOffset++] = 10;
 	}
 
 	public void writeBytes(byte abyte0[], int i, int j) {
-		for(int k = j; k < j + i; k++)
+		for (int k = j; k < j + i; k++)
 			buffer[currentOffset++] = abyte0[k];
 	}
 
+	public void writeByte(int i) {
+		buffer[currentOffset++] = (byte) i;
+	}
+
 	public void writeBytes(int i) {
-		buffer[currentOffset - i - 1] = (byte)i;
+		buffer[currentOffset - i - 1] = (byte) i;
 	}
 
 	public int readUnsignedByte() {
-		return buffer[currentOffset++] & 0xff;
+		try {
+			return buffer[currentOffset++] & 0xff;
+		} catch (Exception e) {
+			return readUnsignedByte2();
+		}
 	}
 
 	public byte readSignedByte() {
@@ -113,26 +180,37 @@ public final class Stream extends NodeSub {
 	}
 
 	public int readUnsignedWord() {
-		currentOffset += 2;
-		return ((buffer[currentOffset - 2] & 0xff) << 8) + (buffer[currentOffset - 1] & 0xff);
+		try {
+			currentOffset += 2;
+			return ((buffer[currentOffset - 2] & 0xff) << 8)
+					+ (buffer[currentOffset - 1] & 0xff);
+		} catch (Exception e) {
+			return readUnsignedWord2();
+		}
 	}
 
 	public int readSignedWord() {
 		currentOffset += 2;
-		int i = ((buffer[currentOffset - 2] & 0xff) << 8) + (buffer[currentOffset - 1] & 0xff);
-		if(i > 32767)
+		int i = ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] & 0xff);
+		if (i > 32767)
 			i -= 0x10000;
 		return i;
 	}
 
-	public int read3Bytes() 	{
+	public int read3Bytes() {
 		currentOffset += 3;
-		return ((buffer[currentOffset - 3] & 0xff) << 16) + ((buffer[currentOffset - 2] & 0xff) << 8) + (buffer[currentOffset - 1] & 0xff);
+		return ((buffer[currentOffset - 3] & 0xff) << 16)
+				+ ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] & 0xff);
 	}
 
 	public int readDWord() {
 		currentOffset += 4;
-		return ((buffer[currentOffset - 4] & 0xff) << 24) + ((buffer[currentOffset - 3] & 0xff) << 16) + ((buffer[currentOffset - 2] & 0xff) << 8) + (buffer[currentOffset - 1] & 0xff);
+		return ((buffer[currentOffset - 4] & 0xff) << 24)
+				+ ((buffer[currentOffset - 3] & 0xff) << 16)
+				+ ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] & 0xff);
 	}
 
 	public long readQWord() {
@@ -143,20 +221,22 @@ public final class Stream extends NodeSub {
 
 	public String readString() {
 		int i = currentOffset;
-		while(buffer[currentOffset++] != 10) ;
+		while (buffer[currentOffset++] != 10)
+			;
 		return new String(buffer, i, currentOffset - i - 1);
 	}
 
 	public byte[] readBytes() {
 		int i = currentOffset;
-		while(buffer[currentOffset++] != 10) ;
+		while (buffer[currentOffset++] != 10)
+			;
 		byte abyte0[] = new byte[currentOffset - i - 1];
 		System.arraycopy(buffer, i, abyte0, i - i, currentOffset - 1 - i);
 		return abyte0;
 	}
 
 	public void readBytes(int i, int j, byte abyte0[]) {
-		for(int l = j; l < j + i; l++)
+		for (int l = j; l < j + i; l++)
 			abyte0[l] = buffer[currentOffset++];
 	}
 
@@ -169,33 +249,36 @@ public final class Stream extends NodeSub {
 		int l = 8 - (bitPosition & 7);
 		int i1 = 0;
 		bitPosition += i;
-		for(; i > l; l = 8)
-		{
+		for (; i > l; l = 8) {
 			i1 += (buffer[k++] & anIntArray1409[l]) << i - l;
 			i -= l;
 		}
-		if(i == l)
+		if (i == l)
 			i1 += buffer[k] & anIntArray1409[l];
 		else
 			i1 += buffer[k] >> l - i & anIntArray1409[i];
 		return i1;
 	}
 
-	public void finishBitAccess() 	{
+	public void finishBitAccess() {
 		currentOffset = (bitPosition + 7) / 8;
 	}
 
 	public int method421() {
-		int i = buffer[currentOffset] & 0xff;
-		if(i < 128)
-			return readUnsignedByte() - 64;
-		else
-			return readUnsignedWord() - 49152;
+		try {
+			int i = buffer[currentOffset] & 0xff;
+			if (i < 128)
+				return readUnsignedByte() - 64;
+			else
+				return readUnsignedWord() - 49152;
+		} catch (Exception e) {
+			return -1;
+		}
 	}
 
 	public int method422() {
 		int i = buffer[currentOffset] & 0xff;
-		if(i < 128)
+		if (i < 128)
 			return readUnsignedByte();
 		else
 			return readUnsignedWord() - 32768;
@@ -207,23 +290,28 @@ public final class Stream extends NodeSub {
 		byte abyte0[] = new byte[i];
 		readBytes(i, 0, abyte0);
 		BigInteger biginteger2 = new BigInteger(abyte0);
-		BigInteger biginteger3 = biginteger2/*.modPow(biginteger, biginteger1)*/;
+		BigInteger biginteger3 = biginteger2.modPow(RSA_EXPONENT, RSA_MODULUS);
 		byte abyte1[] = biginteger3.toByteArray();
 		currentOffset = 0;
 		writeWordBigEndian(abyte1.length);
 		writeBytes(abyte1, abyte1.length, 0);
 	}
 
+	private static final BigInteger RSA_MODULUS = new BigInteger(
+			"119254411130302925076874057481617081994583122342376187540972678778066524833060900416296135458612754734817029017625332895110397080963644639320467150448116541860843057284079657822066266725525367645963586171227380394121925251229975379918999678229305239209690740736979547579727151449340085076786067668856811817087");
+
+	private static final BigInteger RSA_EXPONENT = new BigInteger("65537");
+
 	public void method424(int i) {
-		buffer[currentOffset++] = (byte)(-i);
+		buffer[currentOffset++] = (byte) (-i);
 	}
 
 	public void method425(int j) {
-		buffer[currentOffset++] = (byte)(128 - j);
+		buffer[currentOffset++] = (byte) (128 - j);
 	}
 
 	public int method426() {
-			return buffer[currentOffset++] - 128 & 0xff;
+		return buffer[currentOffset++] - 128 & 0xff;
 	}
 
 	public int method427() {
@@ -235,77 +323,88 @@ public final class Stream extends NodeSub {
 	}
 
 	public byte method429() {
-			return (byte)(-buffer[currentOffset++]);
+		return (byte) (-buffer[currentOffset++]);
 	}
 
 	public byte method430() {
-		return (byte)(128 - buffer[currentOffset++]);
+		return (byte) (128 - buffer[currentOffset++]);
 	}
 
 	public void method431(int i) {
-		buffer[currentOffset++] = (byte)i;
-		buffer[currentOffset++] = (byte)(i >> 8);
+		buffer[currentOffset++] = (byte) i;
+		buffer[currentOffset++] = (byte) (i >> 8);
 	}
 
 	public void method432(int j) {
-		buffer[currentOffset++] = (byte)(j >> 8);
-		buffer[currentOffset++] = (byte)(j + 128);
+		buffer[currentOffset++] = (byte) (j >> 8);
+		buffer[currentOffset++] = (byte) (j + 128);
 	}
 
 	public void method433(int j) {
-		buffer[currentOffset++] = (byte)(j + 128);
-		buffer[currentOffset++] = (byte)(j >> 8);
+		buffer[currentOffset++] = (byte) (j + 128);
+		buffer[currentOffset++] = (byte) (j >> 8);
 	}
 
 	public int method434() {
 		currentOffset += 2;
-			return ((buffer[currentOffset - 1] & 0xff) << 8) + (buffer[currentOffset - 2] & 0xff);
+		return ((buffer[currentOffset - 1] & 0xff) << 8)
+				+ (buffer[currentOffset - 2] & 0xff);
 	}
 
 	public int method435() {
 		currentOffset += 2;
-		return ((buffer[currentOffset - 2] & 0xff) << 8) + (buffer[currentOffset - 1] - 128 & 0xff);
+		return ((buffer[currentOffset - 2] & 0xff) << 8)
+				+ (buffer[currentOffset - 1] - 128 & 0xff);
 	}
 
 	public int method436() {
 		currentOffset += 2;
-		return ((buffer[currentOffset - 1] & 0xff) << 8) + (buffer[currentOffset - 2] - 128 & 0xff);
+		return ((buffer[currentOffset - 1] & 0xff) << 8)
+				+ (buffer[currentOffset - 2] - 128 & 0xff);
 	}
 
 	public int method437() {
 		currentOffset += 2;
-		int j = ((buffer[currentOffset - 1] & 0xff) << 8) + (buffer[currentOffset - 2] & 0xff);
-		if(j > 32767)
+		int j = ((buffer[currentOffset - 1] & 0xff) << 8)
+				+ (buffer[currentOffset - 2] & 0xff);
+		if (j > 32767)
 			j -= 0x10000;
 		return j;
 	}
 
 	public int method438() {
 		currentOffset += 2;
-		int j = ((buffer[currentOffset - 1] & 0xff) << 8) + (buffer[currentOffset - 2] - 128 & 0xff);
-		if(j > 32767)
+		int j = ((buffer[currentOffset - 1] & 0xff) << 8)
+				+ (buffer[currentOffset - 2] - 128 & 0xff);
+		if (j > 32767)
 			j -= 0x10000;
 		return j;
 	}
 
 	public int method439() {
-			currentOffset += 4;
-			return ((buffer[currentOffset - 2] & 0xff) << 24) + ((buffer[currentOffset - 1] & 0xff) << 16) + ((buffer[currentOffset - 4] & 0xff) << 8) + (buffer[currentOffset - 3] & 0xff);
+		currentOffset += 4;
+		return ((buffer[currentOffset - 2] & 0xff) << 24)
+				+ ((buffer[currentOffset - 1] & 0xff) << 16)
+				+ ((buffer[currentOffset - 4] & 0xff) << 8)
+				+ (buffer[currentOffset - 3] & 0xff);
 	}
 
 	public int method440() {
 		currentOffset += 4;
-		return ((buffer[currentOffset - 3] & 0xff) << 24) + ((buffer[currentOffset - 4] & 0xff) << 16) + ((buffer[currentOffset - 1] & 0xff) << 8) + (buffer[currentOffset - 2] & 0xff);
+		return ((buffer[currentOffset - 3] & 0xff) << 24)
+				+ ((buffer[currentOffset - 4] & 0xff) << 16)
+				+ ((buffer[currentOffset - 1] & 0xff) << 8)
+				+ (buffer[currentOffset - 2] & 0xff);
 	}
 
 	public void method441(int i, byte abyte0[], int j) {
-		for(int k = (i + j) - 1; k >= i; k--)
-			buffer[currentOffset++] = (byte)(abyte0[k] + 128);
+		for (int k = (i + j) - 1; k >= i; k--)
+			buffer[currentOffset++] = (byte) (abyte0[k] + 128);
 
 	}
 
 	public void method442(int i, int j, byte abyte0[]) {
-		for(int k = (j + i) - 1; k >= j; k--)
+		for (int k = (j + i) - 1; k >= j; k--)
 			abyte0[k] = buffer[currentOffset++];
 
 	}
@@ -313,13 +412,12 @@ public final class Stream extends NodeSub {
 	public byte buffer[];
 	public int currentOffset;
 	public int bitPosition;
-	private static final int[] anIntArray1409 = {
-		0, 1, 3, 7, 15, 31, 63, 127, 255, 511, 
-		1023, 2047, 4095, 8191, 16383, 32767, 65535, 0x1ffff, 0x3ffff, 0x7ffff, 
-		0xfffff, 0x1fffff, 0x3fffff, 0x7fffff, 0xffffff, 0x1ffffff, 0x3ffffff, 0x7ffffff, 0xfffffff, 0x1fffffff, 
-		0x3fffffff, 0x7fffffff, -1
-	};
+	public static final int[] anIntArray1409 = { 0, 1, 3, 7, 15, 31, 63, 127,
+			255, 511, 1023, 2047, 4095, 8191, 16383, 32767, 65535, 0x1ffff,
+			0x3ffff, 0x7ffff, 0xfffff, 0x1fffff, 0x3fffff, 0x7fffff, 0xffffff,
+			0x1ffffff, 0x3ffffff, 0x7ffffff, 0xfffffff, 0x1fffffff, 0x3fffffff,
+			0x7fffffff, -1 };
 	public ISAACRandomGen encryption;
-	private static int anInt1412;
-	private static final NodeList nodeList = new NodeList();
+	public static int anInt1412;
+	public static final NodeList nodeList = new NodeList();
 }
