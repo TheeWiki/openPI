912d1698876b1088a30c7b0a8eb1876e2e9b9d71
TheeWiki <rare_m3@hotmail.com>
2016-02-11 11:54:56 -0500

    updates

--- Source/src/server/model/npcs/NPC.java
+++ Source/src/server/model/npcs/NPC.java
@@ -14,14 +14,15 @@ public class NPC {
 	public int heightLevel;
 	public int makeX, makeY, maxHit, defence, attack, moveX, moveY, direction, walkingType;
 	public int spawnX, spawnY;
-    public int viewX, viewY;
+	public int viewX, viewY;
 	public int hp, maxHP;
 	public long singleCombatDelay = 0;
-	
+
 	/**
 	 * attackType: 0 = melee, 1 = range, 2 = mage
 	 */
-	public int attackType, projectileId, endGfx, spawnedBy, hitDelayTimer, HP, MaxHP, hitDiff, animNumber, actionTimer, enemyX, enemyY;
+	public int attackType, projectileId, endGfx, spawnedBy, hitDelayTimer, HP, MaxHP, hitDiff, animNumber, actionTimer,
+			enemyX, enemyY;
 	public boolean applyDead, isDead, needRespawn, respawns;
 	public boolean walkingHome, underAttack;
 	public int freezeTimer, attackTimer, killerId, killedBy, oldIndex, underAttackBy;
@@ -32,10 +33,10 @@ public class NPC {
 	public boolean hitUpdateRequired;
 	public boolean updateRequired;
 	public boolean forcedChatRequired;
-    public boolean faceToUpdateRequired;
+	public boolean faceToUpdateRequired;
 	public int firstAttacker;
 	public String forcedText;
-	
+
 	public NPC(int _npcId, int _npcType) {
 		npcId = _npcId;
 		npcType = _npcType;
@@ -43,31 +44,39 @@ public class NPC {
 		isDead = false;
 		applyDead = false;
 		actionTimer = 0;
-		randomWalk = true;		
+		randomWalk = true;
 	}
+
 	public void faceTo(int playerId) {
-		//this.rawDirection = 32768 + playerId;
+		// this.rawDirection = 32768 + playerId;
 		this.dirUpdateRequired = true;
 		this.updateRequired = true;
 	}
-	
+
+	public void dealDamage(int damage) {
+		if (damage > HP) {
+			damage = HP;
+		}
+		HP -= damage;
+	}
+
 	public void updateNPCMovement(Stream str) {
 		if (direction == -1) {
-			
+
 			if (updateRequired) {
-				
+
 				str.writeBits(1, 1);
 				str.writeBits(2, 0);
 			} else {
 				str.writeBits(1, 0);
 			}
 		} else {
-			
+
 			str.writeBits(1, 1);
-			str.writeBits(2, 1);		
+			str.writeBits(2, 1);
 			str.writeBits(3, Misc.xlateDirectionToClient[direction]);
 			if (updateRequired) {
-				str.writeBits(1, 1);		
+				str.writeBits(1, 1);
 			} else {
 				str.writeBits(1, 0);
 			}
@@ -75,79 +84,79 @@ public class NPC {
 	}
 
 	/**
-	* Text update
-	**/
-	
+	 * Text update
+	 **/
+
 	public void forceChat(String text) {
 		forcedText = text;
 		forcedChatRequired = true;
 		updateRequired = true;
 	}
-	
+
 	/**
 	 * @return the npcId
 	 */
 	public int getNpcId() {
 		return npcId;
 	}
-	
+
 	/**
-	*Graphics
-	**/	
-	
+	 * Graphics
+	 **/
+
 	public int mask80var1 = 0;
-    public int mask80var2 = 0;
-    protected boolean mask80update = false;
-	
-    public void appendMask80Update(Stream str) {
+	public int mask80var2 = 0;
+	protected boolean mask80update = false;
+
+	public void appendMask80Update(Stream str) {
 		str.writeWord(mask80var1);
-	    str.writeDWord(mask80var2);
-    }
-	
-	public void gfx100(int gfx){
+		str.writeDWord(mask80var2);
+	}
+
+	public void gfx100(int gfx) {
 		mask80var1 = gfx;
-        mask80var2 = 6553600;
-        mask80update = true;
+		mask80var2 = 6553600;
+		mask80update = true;
 		updateRequired = true;
 	}
-	
-	public void gfx0(int gfx){
+
+	public void gfx0(int gfx) {
 		mask80var1 = gfx;
-        mask80var2 = 65536;
-        mask80update = true;
+		mask80var2 = 65536;
+		mask80update = true;
 		updateRequired = true;
 	}
-	
+
 	public void appendAnimUpdate(Stream str) {
 		str.writeWordBigEndian(animNumber);
 		str.writeByte(1);
 	}
-	
+
 	/**
-	*
-	Face
-	*
-	**/
-	
+	 *
+	 * Face
+	 *
+	 **/
+
 	public int FocusPointX = -1, FocusPointY = -1;
 	public int face = 0;
-	
-	private void appendSetFocusDestination(Stream str) {
-        str.writeWordBigEndian(FocusPointX);
-        str.writeWordBigEndian(FocusPointY);
-    }
-	
-	public void turnNpc(int i, int j) {
-        FocusPointX = 2 * i + 1;
-        FocusPointY = 2 * j + 1;
-        updateRequired = true;
 
-    }
-	
+	private void appendSetFocusDestination(Stream str) {
+		str.writeWordBigEndian(FocusPointX);
+		str.writeWordBigEndian(FocusPointY);
+	}
+
+	public void turnNpc(int i, int j) {
+		FocusPointX = 2 * i + 1;
+		FocusPointY = 2 * j + 1;
+		updateRequired = true;
+
+	}
+
 	public void appendFaceEntity(Stream str) {
 		str.writeWord(face);
 	}
-        	
+
 	public void facePlayer(int player) {
 		face = player + 32768;
 		dirUpdateRequired = true;
@@ -155,34 +164,47 @@ public class NPC {
 	}
 
 	public void appendFaceToUpdate(Stream str) {
-			str.writeWordBigEndian(viewX);
-			str.writeWordBigEndian(viewY);
+		str.writeWordBigEndian(viewX);
+		str.writeWordBigEndian(viewY);
 	}
-	
-	
+
 	public void appendNPCUpdateBlock(Stream str) {
-		if(!updateRequired) return ;		
+		if (!updateRequired)
+			return;
 		int updateMask = 0;
-		if(animUpdateRequired) updateMask |= 0x10; 
-		if(hitUpdateRequired2) updateMask |= 8;
-		if(mask80update) updateMask |= 0x80;
-		if(dirUpdateRequired) updateMask |= 0x20;
-		if(forcedChatRequired) updateMask |= 1;
-		if(hitUpdateRequired) updateMask |= 0x40;		
-		if(FocusPointX != -1) updateMask |= 4;		
-			
+		if (animUpdateRequired)
+			updateMask |= 0x10;
+		if (hitUpdateRequired2)
+			updateMask |= 8;
+		if (mask80update)
+			updateMask |= 0x80;
+		if (dirUpdateRequired)
+			updateMask |= 0x20;
+		if (forcedChatRequired)
+			updateMask |= 1;
+		if (hitUpdateRequired)
+			updateMask |= 0x40;
+		if (FocusPointX != -1)
+			updateMask |= 4;
+
 		str.writeByte(updateMask);
-				
-		if (animUpdateRequired) appendAnimUpdate(str);
-		if (hitUpdateRequired2) appendHitUpdate2(str);
-		if (mask80update)       appendMask80Update(str);
-		if (dirUpdateRequired)  appendFaceEntity(str);
-		if(forcedChatRequired) {
+
+		if (animUpdateRequired)
+			appendAnimUpdate(str);
+		if (hitUpdateRequired2)
+			appendHitUpdate2(str);
+		if (mask80update)
+			appendMask80Update(str);
+		if (dirUpdateRequired)
+			appendFaceEntity(str);
+		if (forcedChatRequired) {
 			str.writeString(forcedText);
 		}
-		if (hitUpdateRequired)  appendHitUpdate(str);
-		if(FocusPointX != -1) appendSetFocusDestination(str);
-		
+		if (hitUpdateRequired)
+			appendHitUpdate(str);
+		if (FocusPointX != -1)
+			appendSetFocusDestination(str);
+
 	}
 
 	public void clearUpdateFlags() {
@@ -206,11 +228,12 @@ public class NPC {
 		animUpdateRequired = true;
 		updateRequired = true;
 	}
-	
+
 	public int getNextWalkingDirection() {
 		int dir;
 		dir = Misc.direction(absX, absY, (absX + moveX), (absY + moveY));
-		if(dir == -1) return -1;
+		if (dir == -1)
+			return -1;
 		dir >>= 1;
 		absX += moveX;
 		absY += moveY;
@@ -220,91 +243,90 @@ public class NPC {
 	@SuppressWarnings("static-access")
 	public void getNextNPCMovement(int i) {
 		direction = -1;
-		if(Server.npcHandler.npcs[i].freezeTimer == 0) {
+		if (Server.npcHandler.npcs[i].freezeTimer == 0) {
 			direction = getNextWalkingDirection();
 		}
 	}
 
-
-	public void appendHitUpdate(Stream str) {		
+	public void appendHitUpdate(Stream str) {
 		if (HP <= 0) {
 			isDead = true;
 		}
-		str.writeByteC(hitDiff); 
+		str.writeByteC(hitDiff);
 		if (hitDiff > 0) {
-			str.writeByteS(1); 
+			str.writeByteS(1);
 		} else {
-			str.writeByteS(0); 
-		}	
-		str.writeByteS(HP); 
-		str.writeByteC(MaxHP); 	
+			str.writeByteS(0);
+		}
+		str.writeByteS(HP);
+		str.writeByteC(MaxHP);
 	}
-	
+
 	public int hitDiff2 = 0;
 	public boolean hitUpdateRequired2 = false;
-	
-	public void appendHitUpdate2(Stream str) {		
+
+	public void appendHitUpdate2(Stream str) {
 		if (HP <= 0) {
 			isDead = true;
 		}
-		str.writeByteA(hitDiff2); 
+		str.writeByteA(hitDiff2);
 		if (hitDiff2 > 0) {
-			str.writeByteC(1); 
+			str.writeByteC(1);
 		} else {
-			str.writeByteC(0); 
-		}	
-		str.writeByteA(HP); 
-		str.writeByte(MaxHP); 	
+			str.writeByteC(0);
+		}
+		str.writeByteA(HP);
+		str.writeByte(MaxHP);
 	}
+
 	@SuppressWarnings("static-access")
 	public static int MAX_NPCS = Server.npcHandler.maxNPCs;
-	
+
 	public void handleHitMask(int damage) {
 		if (!hitUpdateRequired) {
 			hitUpdateRequired = true;
 			hitDiff = damage;
 		} else if (!hitUpdateRequired2) {
 			hitUpdateRequired2 = true;
-			hitDiff2 = damage;		
+			hitDiff2 = damage;
 		}
 		updateRequired = true;
 	}
-	
+
 	public int getX() {
 		return absX;
 	}
-	
+
 	public int getY() {
 		return absY;
 	}
-	
-/*	public NPCDefinition getDefinition() {
-		return definition;
-	}*/
 
-	
+	/*
+	 * public NPCDefinition getDefinition() { return definition; }
+	 */
+
 	public boolean inMulti() {
-		if((absX >= 3136 && absX <= 3327 && absY >= 3519 && absY <= 3607) || 
-			(absX >= 3190 && absX <= 3327 && absY >= 3648 && absY <= 3839) ||  
-			(absX >= 3200 && absX <= 3390 && absY >= 3840 && absY <= 3967) || 
-			(absX >= 2992 && absX <= 3007 && absY >= 3912 && absY <= 3967) || 
-			(absX >= 2946 && absX <= 2959 && absY >= 3816 && absY <= 3831) || 
-			(absX >= 3008 && absX <= 3199 && absY >= 3856 && absY <= 3903) || 
-			(absX >= 3008 && absX <= 3071 && absY >= 3600 && absY <= 3711) || 
-			(absX >= 3072 && absX <= 3327 && absY >= 3608 && absY <= 3647) ||
-			(absX >= 2624 && absX <= 2690 && absY >= 2550 && absY <= 2619) ||
-			(absX >= 2371 && absX <= 2422 && absY >= 5062 && absY <= 5117) ||
-			(absX >= 2896 && absX <= 2927 && absY >= 3595 && absY <= 3630) ||
-			(absX >= 2892 && absX <= 2932 && absY >= 4435 && absY <= 4464) ||
-			(absX >= 2256 && absX <= 2287 && absY >= 4680 && absY <= 4711)) {
+		if ((absX >= 3136 && absX <= 3327 && absY >= 3519 && absY <= 3607)
+				|| (absX >= 3190 && absX <= 3327 && absY >= 3648 && absY <= 3839)
+				|| (absX >= 3200 && absX <= 3390 && absY >= 3840 && absY <= 3967)
+				|| (absX >= 2992 && absX <= 3007 && absY >= 3912 && absY <= 3967)
+				|| (absX >= 2946 && absX <= 2959 && absY >= 3816 && absY <= 3831)
+				|| (absX >= 3008 && absX <= 3199 && absY >= 3856 && absY <= 3903)
+				|| (absX >= 3008 && absX <= 3071 && absY >= 3600 && absY <= 3711)
+				|| (absX >= 3072 && absX <= 3327 && absY >= 3608 && absY <= 3647)
+				|| (absX >= 2624 && absX <= 2690 && absY >= 2550 && absY <= 2619)
+				|| (absX >= 2371 && absX <= 2422 && absY >= 5062 && absY <= 5117)
+				|| (absX >= 2896 && absX <= 2927 && absY >= 3595 && absY <= 3630)
+				|| (absX >= 2892 && absX <= 2932 && absY >= 4435 && absY <= 4464)
+				|| (absX >= 2256 && absX <= 2287 && absY >= 4680 && absY <= 4711)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean inWild() {
-		if(absX > 2941 && absX < 3392 && absY > 3518 && absY < 3966 ||
-			absX > 2941 && absX < 3392 && absY > 9918 && absY < 10366) {	
+		if (absX > 2941 && absX < 3392 && absY > 3518 && absY < 3966
+				|| absX > 2941 && absX < 3392 && absY > 9918 && absY < 10366) {
 			return true;
 		}
 		return false;
