797b0ebd15ef3639921875a73d3e251ebc23f4f1
TheeWiki <hdtitanyt@gmail.com>
2016-02-26 19:57:00 -0500

    cleaned

--- Source/src/server/model/dialogues/DialogueHandler.java
+++ Source/src/server/model/dialogues/DialogueHandler.java
@@ -1,4 +1,6 @@
-package server.model.players;
+package server.model.dialogues;
+
+import server.model.players.Client;
 
 public class DialogueHandler {
 
@@ -23,13 +25,125 @@ public class DialogueHandler {
 			break;
 		case 1:
 			sendStatement("You found a hidden tunnel! Do you want to enter it?");
-			c.dialogueAction = 1;
 			c.nextChat = 2;
 			break;
 		case 2:
-			sendOption2("Yea! I'm fearless!",  "No way! That looks scary!");
-			c.dialogueAction = 1;
-			c.nextChat = 0;
+			DialogueContainer.CreateDialogue(c, new DialogueAction() {
+				@Override
+				public void execute(DialogueContainer container) {
+					switch (container.getOptionId()) {
+					case 1: 
+						c.getPA().spellTeleport(2676, 3715, 0);
+						break;
+					}
+				}
+			}, "I'm fearless, enter now", "Nevermind");
+			break;
+		case 483: //competition judge
+			sendNpcChat4("Hello!", "I'm the competition judge of the Ranging Guild.", "You can buy shots from me and shoot the targets", "for points. You can exchange the points at me.", 693, "Judge");
+			c.nextChat = 484;
+			break;
+			
+		case 484: 
+			sendNpcChat1("What would you like to do/ask?", 693, "Judge");
+			c.nextChat = 485;
+			break;
+			
+		case 485:
+			sendOption4("I would like to buy shots.", "I would like to exchange my points.", "How am I doing right now?", "Never mind.");
+			c.dialogueAction = 485;
+			break;
+			// training
+		case 3:
+			DialogueContainer.CreateDialogue(c, new DialogueAction() {
+				@Override
+				public void execute(DialogueContainer container) {
+					switch (container.getOptionId()) {
+					case 1: 
+						if (container.getCurrentDialogueId() == 2)
+						{
+							c.getPA().spellTeleport(2895, 2727, 0);
+						}
+						c.getPA().spellTeleport(2676, 3715, 0);
+						break;
+					case 2:
+						if (container.getCurrentDialogueId() == 2)
+						{
+							c.getPA().spellTeleport(2710, 9466, 0);
+						}
+						c.getPA().spellTeleport(2737, 3479, 0);
+						break;
+					case 3: 
+						c.getPA().spellTeleport(3428, 3537, 0);
+						break;
+					case 4:
+						c.getPA().spellTeleport(2884, 9798, 0);
+						break;
+					case 5:
+						container.showOptions(2);
+						break;
+					}
+					
+				}
+				@Override
+				public void preExecution(DialogueContainer container) {
+					container.addAdditionalOptions("Lost Island","Brimhaven Dungeon");
+				}
+			}, "Rock Crabs", "Moss Giants", "Slayer Tower", "Taverly Dungeon", "More");
+			break;
+		case 4:
+			DialogueContainer.CreateDialogue(c, new DialogueAction() {
+				@Override
+				public void execute(DialogueContainer container) {
+					switch (container.getOptionId()) {
+					case 1:
+						c.getPA().spellTeleport(3542, 3314, 0);
+						break;
+					case 2: // pc
+						c.getPA().spellTeleport(2658, 2661, 0);
+						break;
+					case 3:
+						c.getPA().spellTeleport(2447, 5171, 0);
+						break;
+					case 4:
+						c.getPA().spellTeleport(2435, 5172, 0);
+						c.sendMessage("Walk west from here.");
+						break;
+					case 5:
+						c.getPA().spellTeleport(2457, 3090, 0);
+						break;
+					}
+				}
+			}, "Barrows","Pest Control", "Fight Caves", "Fight Pits", "Castle Wars");
+			break;
+		case 5:
+			DialogueContainer.CreateDialogue(c, new DialogueAction() {
+				@Override
+				public void execute(DialogueContainer container) {
+					switch (container.getOptionId()) {
+					case 1: // barrows
+						c.getPA().spellTeleport(3542, 3314, 0);
+						break;
+					case 2: // pc
+						c.getPA().spellTeleport(2658, 2661, 0);
+						break;
+					case 3:
+						c.getPA().spellTeleport(2447, 5171, 0);
+						break;
+					case 4:
+						c.getPA().spellTeleport(2435, 5172, 0);
+						c.sendMessage("Walk west from here.");
+						break;
+					case 5:
+						container.showOptions(2);
+						break;
+					}
+				}
+				@Override
+				public void preExecution(DialogueContainer container) {
+					container.addAdditionalOptions("Farming", "Thieving" , "Runecrafting");
+				}
+			}, "Woodcutting","Fishing", "Mining", "Agility", "More");
 			break;
 	}
 }
