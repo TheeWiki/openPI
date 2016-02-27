fd2229679d3bd9ea3f717cfdf88598377826e9ee
TheeWiki <hdtitanyt@gmail.com>
2016-02-26 20:01:01 -0500

    misc

--- Source/src/server/model/players/packet/impl/ClickItem.java
+++ Source/src/server/model/players/packet/impl/ClickItem.java
@@ -2,7 +2,7 @@ package server.model.players.packet.impl;
 
 import server.model.players.Client;
 import server.model.players.packet.PacketType;
-import server.model.players.skills.cooking.Food;
+import server.util.Plugin;
 
 /**
  * Clicking an item, bury bone, eat food etc
@@ -18,51 +18,11 @@ public class ClickItem implements PacketType {
 		if (itemId != c.playerItems[itemSlot] - 1) {
 			return;
 		}
-		if (itemId >= 5509 && itemId <= 5514) {
-			int pouch = -1;
-			int a = itemId;
-			if (a == 5509)
-				pouch = 0;
-			if (a == 5510)
-				pouch = 1;
-			if (a == 5512)
-				pouch = 2;
-			if (a == 5514)
-				pouch = 3;
-			c.getPA().fillPouch(pouch);
-			return;
-		}
-		if (Food.isFood(itemId))
-		{
-			Food.eat(c, itemId, itemSlot);
-		}
-		if (c.getBones().readBone(itemId)){ 
-			c.getBones().boneOnGround(itemId);
-		}
-		//ScriptManager.callFunc("itemClick_"+itemId, c, itemId, itemSlot);
-		if (c.getPotions().isPotion(itemId))
-			c.getPotions().handlePotion(itemId,itemSlot);
-		if (itemId == 952) {
-			if(c.inArea(3553, 3301, 3561, 3294)) {
-				c.teleTimer = 3;
-				c.newLocation = 1;
-			} else if(c.inArea(3550, 3287, 3557, 3278)) {
-				c.teleTimer = 3;
-				c.newLocation = 2;
-			} else if(c.inArea(3561, 3292, 3568, 3285)) {
-				c.teleTimer = 3;
-				c.newLocation = 3;
-			} else if(c.inArea(3570, 3302, 3579, 3293)) {
-				c.teleTimer = 3;
-				c.newLocation = 4;
-			} else if(c.inArea(3571, 3285, 3582, 3278)) {
-				c.teleTimer = 3;
-				c.newLocation = 5;
-			} else if(c.inArea(3562, 3279, 3569, 3273)) {
-				c.teleTimer = 3;
-				c.newLocation = 6;
-			}
-		}
+		/**
+		 * Gets the static usage from the skill class and access it in the Python script
+		 */
+		Plugin.execute("click", c, itemId, itemSlot);
+		
+		Plugin.execute("itemClick_" + itemId, c, itemId, itemSlot);
 	}
-
 }
