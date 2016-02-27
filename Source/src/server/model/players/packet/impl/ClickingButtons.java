41dfd1c3d68be24fdd62b0c9c985cc23f25a3eb7
TheeWiki <hdtitanyt@gmail.com>
2016-02-26 19:52:41 -0500

    misc

--- Source/src/server/model/players/packet/impl/ClickingButtons.java
+++ Source/src/server/model/players/packet/impl/ClickingButtons.java
@@ -6,9 +6,13 @@ import server.event.CycleEvent;
 import server.event.CycleEventContainer;
 import server.event.CycleEventHandler;
 import server.model.items.GameItem;
+import server.model.minigames.duel_arena.Rules;
 import server.model.players.Client;
+import server.model.players.EquipmentListener;
 import server.model.players.packet.PacketType;
+import server.model.players.skills.magic.Enchantment;
 import server.util.Misc;
+import server.util.Plugin;
 import server.world.Location;
 
 /**
@@ -26,163 +30,28 @@ public class ClickingButtons implements PacketType {
 		if (c.playerRights >= 2)
 			Misc.println(c.playerName + " - actionbutton: " + actionButtonId);
 
-		c.getEmoteHandler().startEmote(actionButtonId);
+		Plugin.execute("buttons", c, actionButtonId);
+
 		switch (actionButtonId) {
+		case 485:
+			c.getRG().exchangePoints();
+			break;
 
 		case 150:
-			if (c.autoRet == 0)
 				c.autoRet = 1;
-			else
+				break;
+		case 151:
 				c.autoRet = 0;
 			break;
-		// 1st tele option
-		case 9190:
-			if (c.teleAction == 1) {
-				// rock crabs
-				c.getPA().spellTeleport(2676, 3715, 0);
-			} else if (c.teleAction == 2) {
-				// barrows
-				c.getPA().spellTeleport(3565, 3314, 0);
-			} else if (c.teleAction == 3) {
-				// godwars
-				c.getPA().spellTeleport(2916, 3612, 0);
-			} else if (c.teleAction == 4) {
-				// varrock wildy
-				c.getPA().spellTeleport(3243, 3513, 0);
-			} else if (c.teleAction == 5) {
-				c.getPA().spellTeleport(3046, 9779, 0);
-			}
-
-			if (c.dialogueAction == 10) {
-				c.getPA().spellTeleport(2845, 4832, 0);
-				c.dialogueAction = -1;
-			} else if (c.dialogueAction == 11) {
-				c.getPA().spellTeleport(2786, 4839, 0);
-				c.dialogueAction = -1;
-			} else if (c.dialogueAction == 12) {
-				c.getPA().spellTeleport(2398, 4841, 0);
-				c.dialogueAction = -1;
-			}
-			break;
-		// mining - 3046,9779,0
-		// smithing - 3079,9502,0
-
-		// 2nd tele option
-		case 9191:
-			if (c.teleAction == 1) {
-				// tav dungeon
-				c.getPA().spellTeleport(2884, 9798, 0);
-			} else if (c.teleAction == 2) {
-				// pest control
-				c.getPA().spellTeleport(2662, 2650, 0);
-			} else if (c.teleAction == 3) {
-				// kbd
-				c.getPA().spellTeleport(3007, 3849, 0);
-			} else if (c.teleAction == 4) {
-				// graveyard
-				c.getPA().spellTeleport(3164, 3685, 0);
-			} else if (c.teleAction == 5) {
-				c.getPA().spellTeleport(3079, 9502, 0);
-			}
-			if (c.dialogueAction == 10) {
-				c.getPA().spellTeleport(2796, 4818, 0);
-				c.dialogueAction = -1;
-			} else if (c.dialogueAction == 11) {
-				c.getPA().spellTeleport(2527, 4833, 0);
-				c.dialogueAction = -1;
-			} else if (c.dialogueAction == 12) {
-				c.getPA().spellTeleport(2464, 4834, 0);
-				c.dialogueAction = -1;
-			}
-			break;
-		// 3rd tele option
-
-		case 9192:
-			if (c.teleAction == 1) {
-				// slayer tower
-				c.getPA().spellTeleport(3428, 3537, 0);
-			} else if (c.teleAction == 2) {
-				// tzhaar
-				c.getPA().spellTeleport(2444, 5170, 0);
-			} else if (c.teleAction == 3) {
-				// dag kings
-				c.getPA().spellTeleport(2479, 10147, 0);
-			} else if (c.teleAction == 4) {
-				// 44 portals
-				c.getPA().spellTeleport(2975, 3873, 0);
-			} else if (c.teleAction == 5) {
-				c.getPA().spellTeleport(2813, 3436, 0);
-			}
-			if (c.dialogueAction == 10) {
-				c.getPA().spellTeleport(2713, 4836, 0);
-				c.dialogueAction = -1;
-			} else if (c.dialogueAction == 11) {
-				c.getPA().spellTeleport(2162, 4833, 0);
-				c.dialogueAction = -1;
-			} else if (c.dialogueAction == 12) {
-				c.getPA().spellTeleport(2207, 4836, 0);
-				c.dialogueAction = -1;
-			}
-			break;
-		// 4th tele option
-		case 9193:
-			if (c.teleAction == 1) {
-				// brimhaven dungeon
-				c.getPA().spellTeleport(2710, 9466, 0);
-			} else if (c.teleAction == 2) {
-				// duel arena
-				c.getPA().spellTeleport(3366, 3266, 0);
-			} else if (c.teleAction == 3) {
-				// chaos elemental
-				c.getPA().spellTeleport(3295, 3921, 0);
-			} else if (c.teleAction == 4) {
-				// gdz
-				c.getPA().spellTeleport(3288, 3886, 0);
-			} else if (c.teleAction == 5) {
-				c.getPA().spellTeleport(2724, 3484, 0);
-				c.sendMessage("For magic logs, try north of the duel arena.");
-			}
-			if (c.dialogueAction == 10) {
-				c.getPA().spellTeleport(2660, 4839, 0);
-				c.dialogueAction = -1;
-			}
-			break;
-		// 5th tele option
-		case 9194:
-			if (c.teleAction == 1) {
-				// island
-				c.getPA().spellTeleport(2895, 2727, 0);
-			} else if (c.teleAction == 2) {
-				// last minigame spot
-				c.sendMessage("Suggest something for this spot on the forums!");
-				c.getPA().closeAllWindows();
-			} else if (c.teleAction == 3) {
-				// last monster spot
-				c.sendMessage("Suggest something for this spot on the forums!");
-				c.getPA().closeAllWindows();
-			} else if (c.teleAction == 4) {
-				// ardy lever
-				c.getPA().spellTeleport(2561, 3311, 0);
-			} else if (c.teleAction == 5) {
-				c.getPA().spellTeleport(2812, 3463, 0);
-			}
-			if (c.dialogueAction == 10 || c.dialogueAction == 11) {
-				c.dialogueId++;
-				c.getDH().sendDialogues(c.dialogueId, 0);
-			} else if (c.dialogueAction == 12) {
-				c.dialogueId = 17;
-				c.getDH().sendDialogues(c.dialogueId, 0);
-			}
-			break;
-
-		case 71074:
-			if (c.clanId >= 0) {
-				if (Server.clanChat.clans[c.clanId].owner.equalsIgnoreCase(c.playerName)) {
-					Server.clanChat.sendLootShareMessage(c.clanId, "Lootshare has been toggled to "
-							+ (!Server.clanChat.clans[c.clanId].lootshare ? "on" : "off") + " by the clan leader.");
-					Server.clanChat.clans[c.clanId].lootshare = !Server.clanChat.clans[c.clanId].lootshare;
-				} else
-					c.sendMessage("Only the owner of the clan has the power to do that.");
+			
+		case 9190: // 1st option (5)
+		case 9191: // 2nd option (5)
+		case 9192: // 3rd option (5)
+		case 9193: // 4th option (5)
+		case 9194: // 5th option (5)
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9190) + 1);
+				return;
 			}
 			break;
 
@@ -190,105 +59,61 @@ public class ClickingButtons implements PacketType {
 			// c.getPA().showInterface(15106);
 			c.getItems().writeBonus();
 			break;
-
-		case 59004:
-			c.getPA().removeAllWindows();
-			break;
-
-		case 70212:
-			if (c.clanId > -1)
-				Server.clanChat.leaveClan(c.playerId, c.clanId);
-			else
-				c.sendMessage("You are not in a clan.");
-			break;
-		case 62137:
-			if (c.clanId >= 0) {
-				c.sendMessage("You are already in a clan.");
+			
+		case 9178:
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9178) + 1);
+				return;
+			}
+			switch(c.dialogueAction)
+			{
+			case 485:
+				c.getRG().buyArrows();
+				break;
+			case 2:
+				c.getPA().startTeleport(3428, 3538, 0, "modern");	
 				break;
 			}
-			if (c.getOutStream() != null) {
-				c.getOutStream().createFrame(187);
-				c.flushOutStream();
-			}
-			break;
-
-		case 9178:
-			if (c.usingGlory)
-				c.getPA().startTeleport(Constants.EDGEVILLE_X, Constants.EDGEVILLE_Y, 0, "modern");
-			if (c.dialogueAction == 2)
-				c.getPA().startTeleport(3428, 3538, 0, "modern");
-			if (c.dialogueAction == 3)
-				c.getPA().startTeleport(Constants.EDGEVILLE_X, Constants.EDGEVILLE_Y, 0, "modern");
-			if (c.dialogueAction == 4)
-				c.getPA().startTeleport(3565, 3314, 0, "modern");
-			if (c.dialogueAction == 20) {
-				c.getPA().startTeleport(2897, 3618, 4, "modern");
-				c.killCount = 0;
-			} else if (c.teleAction == 2) {
-				// barrows
-				c.getPA().spellTeleport(3565, 3314, 0);
-			}
-
 			break;
 
 		case 9179:
-			if (c.usingGlory)
-				c.getPA().startTeleport(Constants.AL_KHARID_X, Constants.AL_KHARID_Y, 0, "modern");
-			if (c.dialogueAction == 2)
-				c.getPA().startTeleport(2884, 3395, 0, "modern");
-			if (c.dialogueAction == 3)
-				c.getPA().startTeleport(3243, 3513, 0, "modern");
-			if (c.dialogueAction == 4)
-				c.getPA().startTeleport(2444, 5170, 0, "modern");
-			if (c.dialogueAction == 20) {
-				c.getPA().startTeleport(2897, 3618, 12, "modern");
-				c.killCount = 0;
-			} else if (c.teleAction == 2) {
-				// assault
-				c.getPA().spellTeleport(2605, 3153, 0);
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9178) + 1);
+				return;
 			}
-
+			switch(c.dialogueAction) {
+			case 485:
+				c.getRG().exchangePoints();
+				break;
+			case 2:
+				c.getPA().startTeleport(2884, 3395, 0, "modern");	
+				break;
+			}
 			break;
 
 		case 9180:
-			if (c.usingGlory)
-				c.getPA().startTeleport(Constants.KARAMJA_X, Constants.KARAMJA_Y, 0, "modern");
-			if (c.dialogueAction == 2)
-				c.getPA().startTeleport(2471, 10137, 0, "modern");
-			if (c.dialogueAction == 3)
-				c.getPA().startTeleport(3363, 3676, 0, "modern");
-			if (c.dialogueAction == 4)
-				c.getPA().startTeleport(2659, 2676, 0, "modern");
-			if (c.dialogueAction == 20) {
-				c.getPA().startTeleport(2897, 3618, 8, "modern");
-				c.killCount = 0;
-			} else if (c.teleAction == 2) {
-				// duel arena
-				c.getPA().spellTeleport(3366, 3266, 0);
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9178) + 1);
+				return;
 			}
-
+			switch(c.dialogueAction) {
+			case 485:
+				c.getRG().howAmIDoing();
+				break;
+				
+			}
 			break;
 
 		case 9181:
-			if (c.usingGlory)
-				c.getPA().startTeleport(Constants.MAGEBANK_X, Constants.MAGEBANK_Y, 0, "modern");
-			if (c.dialogueAction == 2)
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9178) + 1);
+				return;
+			}
+			switch(c.dialogueAction) {
+			case 2:
 				c.getPA().startTeleport(2669, 3714, 0, "modern");
-			if (c.dialogueAction == 3)
-				c.getPA().startTeleport(2540, 4716, 0, "modern");
-			if (c.dialogueAction == 4) {
-				c.getPA().startTeleport(3366, 3266, 0, "modern");
-				c.sendMessage("Dueling is at your own risk. Refunds will not be given for items lost due to glitches.");
-			} else if (c.teleAction == 2) {
-				// tzhaar
-				c.getPA().spellTeleport(2444, 5170, 0);
+				break;
 			}
-			if (c.dialogueAction == 20) {
-				// c.getPA().startTeleport(3366, 3266, 0, "modern");
-				// c.killCount = 0;
-				c.sendMessage("This will be added shortly");
-			}
-
 			break;
 
 		case 1093:
@@ -299,12 +124,12 @@ public class ClickingButtons implements PacketType {
 			} else {
 	
 				if (c.playerMagicBook == 1) {
-					if (c.playerEquipment[c.playerWeapon] == 4675)
+					if (c.playerEquipment[EquipmentListener.WEAPON_SLOT.getSlot()] == 4675)
 						c.setSidebarInterface(0, 1689);
 					else
 						c.sendMessage("You can't autocast ancients without an ancient staff.");
 				} else if (c.playerMagicBook == 0) {
-					if (c.playerEquipment[c.playerWeapon] == 4170) {
+					if (c.playerEquipment[EquipmentListener.WEAPON_SLOT.getSlot()] == 4170) {
 						c.setSidebarInterface(0, 12050);
 					} else {
 						c.setSidebarInterface(0, 1829);
@@ -318,7 +143,13 @@ public class ClickingButtons implements PacketType {
 //			if (c.dialogueAction == 1) {
 //				c.getDH().sendDialogues(38, -1);
 //			}
-			if (c.dialogueAction == 1) {
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9157) + 1);
+				return;
+			}
+			switch(c.dialogueAction)
+			{
+			case 1:
 				int r = 4;
 				// int r = Misc.random(3);
 				switch (r) {
@@ -342,31 +173,26 @@ public class ClickingButtons implements PacketType {
 					break;
 				}
 			}
-			break;
-
-		case 9167:
-			
-			break;
-		case 9168:
-
-
-			break;
-		case 9169:
-
-			break;
-
+		break;
 		case 9158:
-
-			if (c.dialogueAction == 8) {
-				c.getPA().fixAllBarrows();
-			} else {
-				c.dialogueAction = 0;
-				c.getPA().removeAllWindows();
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9157) + 1);
+				return;
 			}
-			if (c.dialogueAction == 27) {
-				c.getPA().removeAllWindows();
+			break;
+		case 9058:
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9157) + 1);
+				return;
 			}
-
+			break;
+		case 9167: // 1st option (3)
+		case 9168: // 2nd option (3)
+		case 9169: // 3rd option (3)
+			if (c.dialogueContainer != null) {
+				c.dialogueContainer.execute((actionButtonId - 9167) + 1);
+				return;
+			}
 			break;
 
 		/** Specials **/
@@ -401,7 +227,7 @@ public class ClickingButtons implements PacketType {
 			break;
 
 		case 29063:
-			if (c.getCombat().checkSpecAmount(c.playerEquipment[c.playerWeapon])) {
+			if (c.getCombat().checkSpecAmount(c.playerEquipment[EquipmentListener.WEAPON_SLOT.getSlot()])) {
 				c.gfx0(246);
 				c.forcedChat("Raarrrrrgggggghhhhhhh!");
 				c.startAnimation(1056);
@@ -577,7 +403,7 @@ public class ClickingButtons implements PacketType {
 					return;
 				}
 
-				if (c.duelRule[2] && c.duelRule[3] && c.duelRule[4]) {
+				if (c.duelRule[Rules.RANGE_RULE.getRule()] && c.duelRule[Rules.MELEE_RULE.getRule()] && c.duelRule[Rules.MAGIC_RULE.getRule()]) {
 					c.sendMessage("You won't be able to attack the player with the rules you have set.");
 					break;
 				}
@@ -659,21 +485,19 @@ public class ClickingButtons implements PacketType {
 			}
 			c.godSpellDelay = System.currentTimeMillis();
 			c.sendMessage("You feel charged with a magical power!");
-			c.gfx100(c.MAGIC_SPELLS[48][3]);
-			c.startAnimation(c.MAGIC_SPELLS[48][2]);
+			c.gfx100(Enchantment.MAGIC_SPELLS[48][3]);
+			c.startAnimation(Enchantment.MAGIC_SPELLS[48][2]);
 			c.usingMagic = false;
 			break;
 
 		case 152:
 			c.isRunning2 = !c.isRunning2;
-			int frame = c.isRunning2 == true ? 1 : 0;
-			c.getPA().sendFrame36(173, frame);
+			c.getPA().sendFrame36(173, 0);
 			break;
-
-		case 9154:
-			c.logout();
+		case 153:
+			c.isRunning2 = !c.isRunning2;
+			c.getPA().sendFrame36(173, 1);
 			break;
-
 		case 21010:
 			c.takeAsNote = true;
 			break;
@@ -690,26 +514,25 @@ public class ClickingButtons implements PacketType {
 
 		case 50235:
 		case 4140:
-			c.getPA().startTeleport(Constants.VARROCK_X, Constants.VARROCK_Y, 0, "modern");
+			c.getPA().startTeleport(Constants.RESPAWN_X, Constants.RESPAWN_Y, 0, "modern");
 			c.teleAction = 1;
 			break;
 
 		case 4143:
 		case 50245:
-			c.getPA().startTeleport(Constants.LUMBY_X, Constants.LUMBY_Y, 0, "modern");
-			c.teleAction = 2;
+			// Training dialogue options
+			c.getDH().sendDialogues(3, -1);
 			break;
 
 		case 50253:
 		case 4146:
-			c.getPA().startTeleport(Constants.FALADOR_X, Constants.FALADOR_Y, 0, "modern");
-			c.teleAction = 3;
+			// Minigame dialogue options
+			c.getDH().sendDialogues(4, -1);
 			break;
 
 		case 51005:
 		case 4150:
-			c.getPA().startTeleport(Constants.CAMELOT_X, Constants.CAMELOT_Y, 0, "modern");
-			c.teleAction = 4;
+			c.getDH().sendDialogues(5, -1);
 			break;
 
 		case 51013:
@@ -728,13 +551,6 @@ public class ClickingButtons implements PacketType {
 		case 29031:
 			c.getPA().startTeleport(Constants.TROLLHEIM_X, Constants.TROLLHEIM_Y, 0, "modern");
 			c.teleAction = 7;
-			break;
-
-		case 72038:
-		case 51039:
-			// c.getPA().startTeleport(Config.TROLLHEIM_X, Config.TROLLHEIM_Y,
-			// 0, "modern");
-			// c.teleAction = 8;
 			break;
 
 		case 9125: // Accurate
@@ -796,86 +612,6 @@ public class ClickingButtons implements PacketType {
 			c.fightMode = 2;
 			if (c.autocasting)
 				c.getPA().resetAutocast();
-			break;
-
-		/** Prayers **/
-		case 21233: // thick skin
-			c.getCombat().activatePrayer(0);
-			break;
-		case 21234: // burst of str
-			c.getCombat().activatePrayer(1);
-			break;
-		case 21235: // charity of thought
-			c.getCombat().activatePrayer(2);
-			break;
-		case 70080: // range
-			c.getCombat().activatePrayer(3);
-			break;
-		case 70082: // mage
-			c.getCombat().activatePrayer(4);
-			break;
-		case 21236: // rockskin
-			c.getCombat().activatePrayer(5);
-			break;
-		case 21237: // super human
-			c.getCombat().activatePrayer(6);
-			break;
-		case 21238: // improved reflexes
-			c.getCombat().activatePrayer(7);
-			break;
-		case 21239: // hawk eye
-			c.getCombat().activatePrayer(8);
-			break;
-		case 21240:
-			c.getCombat().activatePrayer(9);
-			break;
-		case 21241: // protect Item
-			c.getCombat().activatePrayer(10);
-			break;
-		case 70084: // 26 range
-			c.getCombat().activatePrayer(11);
-			break;
-		case 70086: // 27 mage
-			c.getCombat().activatePrayer(12);
-			break;
-		case 21242: // steel skin
-			c.getCombat().activatePrayer(13);
-			break;
-		case 21243: // ultimate str
-			c.getCombat().activatePrayer(14);
-			break;
-		case 21244: // incredible reflex
-			c.getCombat().activatePrayer(15);
-			break;
-		case 21245: // protect from magic
-			c.getCombat().activatePrayer(16);
-			break;
-		case 21246: // protect from range
-			c.getCombat().activatePrayer(17);
-			break;
-		case 21247: // protect from melee
-			c.getCombat().activatePrayer(18);
-			break;
-		case 70088: // 44 range
-			c.getCombat().activatePrayer(19);
-			break;
-		case 70090: // 45 mystic
-			c.getCombat().activatePrayer(20);
-			break;
-		case 2171: // retrui
-			c.getCombat().activatePrayer(21);
-			break;
-		case 2172: // redem
-			c.getCombat().activatePrayer(22);
-			break;
-		case 2173: // smite
-			c.getCombat().activatePrayer(23);
-			break;
-		case 70092: // chiv
-			c.getCombat().activatePrayer(24);
-			break;
-		case 70094: // piety
-			c.getCombat().activatePrayer(25);
 			break;
 
 		case 13092:
@@ -966,12 +702,12 @@ public class ClickingButtons implements PacketType {
 		case 17199:
 			c.getPA().removeAllWindows();
 			c.sendMessage("You got the riddle wrong, and it locks back up.");
-			server.model.minigames.Barrows.wrongPuzzle = true;
+			server.model.minigames.barrows.Barrows.wrongPuzzle = true;
 			break;
 		case 17198:
 			c.getPA().removeAllWindows();
 			c.sendMessage("You got the riddle wrong, and it locks back up.");
-			server.model.minigames.Barrows.wrongPuzzle = true;
+			server.model.minigames.barrows.Barrows.wrongPuzzle = true;
 			break;
 		case 13218:
 			c.tradeAccepted = true;
@@ -1133,11 +869,14 @@ public class ClickingButtons implements PacketType {
 		case 24017:
 			c.getPA().resetAutocast();
 			// c.sendFrame246(329, 200, c.playerEquipment[c.playerWeapon]);
-			c.getItems().sendWeapon(c.playerEquipment[c.playerWeapon],
-					c.getItems().getItemName(c.playerEquipment[c.playerWeapon]));
+			c.getItems().sendWeapon(c.playerEquipment[EquipmentListener.WEAPON_SLOT.getSlot()],
+					c.getItems().getItemName(c.playerEquipment[EquipmentListener.WEAPON_SLOT.getSlot()]));
 			// c.setSidebarInterface(0, 328);
 			// c.setSidebarInterface(6, c.playerMagicBook == 0 ? 1151 :
 			// c.playerMagicBook == 1 ? 12855 : 1151);
+			break;
+		default:
+			Plugin.execute("clickButton_" + actionButtonId, c);
 			break;
 		}
 		if (c.isAutoButton(actionButtonId))
