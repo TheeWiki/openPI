63ea7c9f07b117c140cfe55e2c3a51dde0598723
TheeWiki <hdtitanyt@gmail.com>
2016-02-26 19:44:53 -0500

    award skill capes

---
+++ Source/src/server/model/players/skills/AwardSkillcape.java
@@ -0,0 +1,27 @@
+package server.model.players.skills;
+
+import server.model.players.Client;
+
+public class AwardSkillcape {
+
+	/**
+	 * Skill cape awarding
+	 * @param c
+	 * @param arg
+	 */
+	public static void executeAward(Client c) {
+		for (SkillMasters sm : SkillMasters.values()) {
+			if (c.getItems().freeSlots() < 1 ) {
+				c.sendMessage("You don't have enough space");
+				return;
+			}
+			if (!(c.playerLevel[SkillIndex.getSkills(c.getShops().get99Count())] < 99))
+			{
+				c.sendMessage("You need to master all skills first..");
+				return;
+			}
+			c.getDH().sendNpcChat1("Here's you Skillcape & Hood " + c.playerName + ".", sm.getNpcId(), sm.getName());
+			c.nextChat = 0;
+		}
+	}
+}
