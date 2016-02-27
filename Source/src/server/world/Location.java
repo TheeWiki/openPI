1c33383611908436fd25a34c38fbd804a1e49da1
TheeWiki <hdtitanyt@gmail.com>
2016-02-26 20:08:01 -0500

    misc

--- Source/src/server/world/Location.java
+++ Source/src/server/world/Location.java
@@ -7,15 +7,9 @@ import server.event.CycleEventHandler;
 import server.model.players.Client;
 
 public class Location {
-
-	/**
-	 * Usage: new Location(player, x, y, z)
-	 */
-	/*
-	 * Types teleport
-	 */
 	
-	int NORMAL = 0, ANCIENT = 1, LUNAR = 2;
+	@SuppressWarnings("unused")
+	private int NORMAL = 0, ANCIENT = 1, LUNAR = 2;
 	
 	/**
 	 * Handles the teleport
@@ -23,7 +17,6 @@ public class Location {
 	 * @param x x coordinate
 	 * @param y y coordindate
 	 * @param height heightlevel
-	 * @param type teleport type
 	 */
 	public Location(Client player, int x, int y, int height) {
 		this.x = x;
@@ -32,7 +25,7 @@ public class Location {
 		handleTeleport(player);
 	}
 	
-	int x, y, height;
+	private int x, y, height;
 	
 	/*
 	 * Gets the X coordinate
@@ -59,7 +52,7 @@ public class Location {
 	 * Handles the teleport
 	 * @param player player teleporting
 	 */
-	void handleTeleport(final Client player) {
+	public void handleTeleport(final Client player) {
 		if (!canTeleport(player))
 			return;
 		player.teleporting = true;
@@ -76,16 +69,17 @@ public class Location {
 			        player.teleportToY = getY();
 					player.heightLevel = getHeight();
 					player.getPA().requestUpdates();
-					/*if (player.playerMagicBook == 1)
-						player.gfx0(getTeleportEndGfx(player));*/
+					if (player.playerMagicBook == 1)
+						player.gfx0(getTeleportEndGfx(player));
 					player.startAnimation(getTeleportEndAnimation(player));
 				}
-				player.resetShaking();
+				
 				if (timer == 0)
 					container.stop();
 			}
 			@Override
 			public void stop() {
+				player.getPA().resetShaking();
 				player.teleporting = false;
 			}
 		}, 1);
@@ -96,7 +90,7 @@ public class Location {
 	 * @param player player teleporting
 	 * @return teleport gfx
 	 */
-	int getTeleportGfx(Client player) {
+	private int getTeleportGfx(Client player) {
 		return player.playerMagicBook == 0 ? 308 : player.playerMagicBook == 1 ? 0 : 1685; 
 	}
 	
@@ -105,7 +99,7 @@ public class Location {
 	 * @param player player teleporting
 	 * @return teleport animation
 	 */
-	int getTeleportAnimation(Client player) {
+	private int getTeleportAnimation(Client player) {
 		return player.playerMagicBook == 0 ? 714 : player.playerMagicBook == 1 ? 1979 : 9606; 
 	}
 	
@@ -114,7 +108,7 @@ public class Location {
 	 * @param player player teleporting
 	 * @return end animation for teleporting
 	 */
-	int getTeleportEndAnimation(Client player) {
+	private int getTeleportEndAnimation(Client player) {
 		return player.playerMagicBook == 0 ? 715 : 0; 
 	}
 	
@@ -123,7 +117,7 @@ public class Location {
 	 * @param player player teleporting
 	 * @return end animation for teleporting
 	 */
-	int getTeleportEndGfx(Client player) {
+	private int getTeleportEndGfx(Client player) {
 		return /*player.playerMagicBook == 1 ? 8941 : player.playerMagicBook == 1 ? */1681/* : 9013*/; 
 	}
 	
@@ -132,7 +126,7 @@ public class Location {
 	 * @param player player teleporting
 	 * @return can teleport or not
 	 */
-	boolean canTeleport(Client player) {
+	private boolean canTeleport(Client player) {
 		if (player.inWild() && player.wildLevel > Constants.NO_TELEPORT_WILD_LEVEL) {
 			player.sendMessage("You can't teleport above level "+ Constants.NO_TELEPORT_WILD_LEVEL + " in the wilderness.");
 			return false;
